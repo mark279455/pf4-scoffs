cc8a16e07a9a00b91edfaef4e0a72c5931c41025
mark279455 <mark279455@ntlworld.com>
2023-08-13 11:23:09 +0100

    allauth configuration and styling

--- main/settings.py
+++ main/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    # Other
+    # allauth
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -48,6 +48,10 @@ INSTALLED_APPS = [
 
     # Apps
     "home",
+
+    #
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 SITE_ID = 1
@@ -61,6 +65,9 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 ROOT_URLCONF = "main.urls"
 
@@ -79,6 +86,10 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field'
+            ]
         },
     },
 ]
